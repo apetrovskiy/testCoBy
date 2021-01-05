25ea6dc93755d804cf904735b766f9272e32a29f
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-05 17:18:03 +0300

    Python is partially done

--- src/main/java/challenges/MinWindowSubstring/solution_min_window_substring.py
+++ src/main/java/challenges/MinWindowSubstring/solution_min_window_substring.py
@@ -15,14 +15,16 @@ def MinWindowSubstring(strArr):
         while strArr[0].find(strArr[1][char_num], position) > -1:
             search_list.append(strArr[0].find(strArr[1][char_num], position))
             position += 1
+        print(f"for char {strArr[1][char_num]}")
+        print(f"list {search_list}")
         search_list = set(search_list)
         full_search_list.append(search_list)
     # print(full_search_list)
     all_min = max([min(x) for x in full_search_list])
-    # print(all_min)
+    print(all_min)
     all_max = min([max(x) for x in full_search_list])
-    # print(all_max)
-    return strArr[0][all_max:all_min]
+    print(all_max)
+    return strArr[0][all_max:all_min + 1]
 
 
 '''
