562346515382e8b79434a6ffc7d8077ff830b9e1
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-05 06:04:58 +0300

    Update solution_min_window_substring.py

--- src/main/java/challenges/MinWindowSubstring/solution_min_window_substring.py
+++ src/main/java/challenges/MinWindowSubstring/solution_min_window_substring.py
@@ -1,8 +1,10 @@
 def MinWindowSubstring(strArr):
-    
-  # code goes here
-  # return strArr
-  return "ss"
+    # code goes here
+    if strArr is None or len(strArr[1]) == 0:
+        return ""
+    if len(strArr[1]) == 1:
+        return "" if strArr[1] not in strArr[0] else strArr[1]
+    return "ququ"
 
 # keep this function call here 
-print(MinWindowSubstring(input()))
+# print(MinWindowSubstring(input()))
