447ccdd24a41a9f3878d7221f437929f7196d800
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-05 17:14:02 +0300

    Update solution_min_window_substring.py

--- src/main/java/challenges/MinWindowSubstring/solution_min_window_substring.py
+++ src/main/java/challenges/MinWindowSubstring/solution_min_window_substring.py
@@ -1,10 +1,28 @@
 def MinWindowSubstring(strArr):
     # code goes here
+    result = ""
     if strArr is None or len(strArr[1]) == 0:
-        return ""
+        return result
     if len(strArr[1]) == 1:
-        return "" if strArr[1] not in strArr[0] else strArr[1]
-    return "ququ"
+        return result if strArr[1] not in strArr[0] else strArr[1]
+    for index in range(0, len(strArr[1])):
+      if strArr[0].find(strArr[1][index]) == -1:
+        return result
+    full_search_list = []
+    for char_num in range(0, len(strArr[1])):
+        position = 0
+        search_list = []
+        while strArr[0].find(strArr[1][char_num], position) > -1:
+            search_list.append(strArr[0].find(strArr[1][char_num], position))
+            position += 1
+        search_list = set(search_list)
+        full_search_list.append(search_list)
+    # print(full_search_list)
+    all_min = max([min(x) for x in full_search_list])
+    # print(all_min)
+    all_max = min([max(x) for x in full_search_list])
+    # print(all_max)
+    return strArr[0][all_max:all_min]
 
 
 '''
