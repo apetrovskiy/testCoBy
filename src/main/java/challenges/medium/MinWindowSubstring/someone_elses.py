4faee4cd922c994bfa990e1bf1ac6deff86ab7a5
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-09 07:07:56 +0300

    refactoring

---
+++ src/main/java/challenges/medium/MinWindowSubstring/someone_elses.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+EMPTY_COUNTER = Counter()
+
+def MinWindowSubstring1(strArr):
+  N, K = strArr
+  frequencyK = Counter(K)
+  options = []
+  for i in range(len(N)):
+    curr = Counter()
+    for j in range(i, len(N)):
+      curr[N[j]] += 1
+      if frequencyK - curr == EMPTY_COUNTER:
+        options.append(N[i:j + 1])
+        break
+  return min(options, key=len)
---
+++ src/test/java/challenges/medium/MinWindowSubstring/test_min_window_substring.py
@@ -0,0 +1,27 @@
+from src.main.java.challenges.medium.MinWindowSubstring.someone_elses import MinWindowSubstring1
+from src.main.java.challenges.medium.MinWindowSubstring.solution_min_window_substring import MinWindowSubstring
+import pytest
+from typing import List
+
+
+test_data = [
+    (["aaabaaddae", "aed"], "dae"),
+    (["aabdccdbcacd", "aad"], "aabd"),
+    (["ahffaksfajeeubsne", "jefaa"], "aksfaje"),
+    (["aaffhkksemckelloe", "fhea"], "affhkkse"),
+    (["aaffsfsfasfasfasfasfasfacasfafe", "fafe"], "fafe"),
+    (["aaffsfsfasfasfasfasfasfacasfafe", "fafsf"], "affsf"),
+    (["vvavereveaevafefaef", "vvev"], "vvave"),
+    (["caae", "cae"], "caae"),
+    (["cccaabbbbrr", "rbac"], "caabbbbr")
+]
+
+
+@pytest.mark.parametrize("input_array,expected_result", test_data)
+def test_min_window_substring(input_array: List[str], expected_result: str):
+    assert expected_result == MinWindowSubstring(input_array)
+
+
+@pytest.mark.parametrize("input_array,expected_result", test_data)
+def test_min_window_substring_someone_elses(input_array: List[str], expected_result: str):
+    assert expected_result == MinWindowSubstring1(input_array)
