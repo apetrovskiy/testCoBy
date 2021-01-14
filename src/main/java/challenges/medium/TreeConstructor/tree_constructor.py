cead4717fe0e40e0ef985d80f2da8c4123a5f3e0
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-14 19:02:46 +0300

    Python is done

--- src/main/java/challenges/medium/TreeConstructor/tree_constructor.py
+++ src/main/java/challenges/medium/TreeConstructor/tree_constructor.py
@@ -9,7 +9,12 @@ SECOND_NUMBER = re.compile("(?<=\\,)\\d+(?=\\))")
 def TreeConstructor(strArr):
 
     # code goes here
+    TRUE = "true"
     num_array = unpack_to_integers(strArr)
+    if len(num_array) == 1:
+        return TRUE
+    if len(num_array) == 2 and (num_array[0][1] == num_array[1][0] or num_array[1][0] == num_array[0][1]):
+        return TRUE
     result_dict = dict(Counter([item[1] for item in num_array]))
     return str(not any([x for x in result_dict if result_dict[x] > 2]) and any([x for x in result_dict if result_dict[x] == 2])).lower()
 
