cbc0b998a8212a1ce8fa30e59380fc80b81ca335
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-14 04:35:23 +0300

    Update tree_constructor.py

--- src/main/java/challenges/medium/TreeConstructor/tree_constructor.py
+++ src/main/java/challenges/medium/TreeConstructor/tree_constructor.py
@@ -4,18 +4,20 @@ import re
 FIRST_NUMBER = re.compile("(?<=\\()\\d+(?=\\,)")
 SECOND_NUMBER = re.compile("(?<=\\,)\\d+(?=\\))")
 
+
 def TreeConstructor(strArr):
 
     # code goes here
-    print(1111)
-    return strArr
+    numArray = unpack_to_integers(strArr)
+    print(numArray)
+    return numArray
 
 
 def unpack_to_integers(strArray):
-    pass
+    return [[int(FIRST_NUMBER.search(x).group(0)), int(SECOND_NUMBER.search(x).group(0))] for x in strArray]
 
 
 '''
-# keep this function call here 
+# keep this function call here
 print(TreeConstructor(input()))
 '''
