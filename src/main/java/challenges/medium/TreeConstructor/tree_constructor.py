e56db2db83e5c9c8abefa96a355b318d8316cfb0
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-14 05:21:07 +0300

    Python is not yet done

--- src/main/java/challenges/medium/TreeConstructor/tree_constructor.py
+++ src/main/java/challenges/medium/TreeConstructor/tree_constructor.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 
 FIRST_NUMBER = re.compile("(?<=\\()\\d+(?=\\,)")
@@ -8,16 +9,16 @@ SECOND_NUMBER = re.compile("(?<=\\,)\\d+(?=\\))")
 def TreeConstructor(strArr):
 
     # code goes here
-    numArray = unpack_to_integers(strArr)
-    print(numArray)
-    return numArray
+    num_array = unpack_to_integers(strArr)
+    result_dict = dict(Counter([item[1] for item in num_array]))
+    return str(not any([x for x in result_dict if result_dict[x] > 2]) and any([x for x in result_dict if result_dict[x] == 2])).lower()
 
 
-def unpack_to_integers(strArray):
-    return [[int(FIRST_NUMBER.search(x).group(0)), int(SECOND_NUMBER.search(x).group(0))] for x in strArray]
+def unpack_to_integers(str_array):
+    return [[int(FIRST_NUMBER.search(x).group(0)), int(SECOND_NUMBER.search(x).group(0))] for x in str_array]
 
 
 '''
-# keep this function call here
+# keep this function call here 
 print(TreeConstructor(input()))
 '''
